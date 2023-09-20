c9e4d935aa01618c7861ca3ba577d24a13449461
Dimanqe <d.sankeev@gmail.com>
2023-09-20 23:50:35 +0300

    Добавьте файлы проекта.

---
+++ Task 13.6.1 (HW-03)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace CountWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstStopWatch = Stopwatch.StartNew();
+            List<char> characters1 = new List<char>(File.ReadAllText("C:\\Users\\dsank\\Desktop\\Text1.txt").ToCharArray());
+            Console.WriteLine($"Создан List<T>. Время создания: {firstStopWatch.Elapsed.TotalMilliseconds}  мс");
+
+            var secondStopWatch = Stopwatch.StartNew();
+            LinkedList<char> characters2 = new LinkedList<char>(File.ReadAllText("C:\\Users\\dsank\\Desktop\\Text1.txt").ToCharArray());
+            Console.WriteLine($"Создан LinkedList<T>. Время создания: {secondStopWatch.Elapsed.TotalMilliseconds}  мс");
+            Console.WriteLine();            
+        }
+    }
+}
